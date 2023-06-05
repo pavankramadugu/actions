ce448123426632e85e229c9fbd368dcf664a18e0
Pavan Kumar Ramadugu <pavankramadugu@Pavans-MacBook-Air.local>
2023-06-05 09:06:13 -0700

    Added test

--- tests.java
+++ tests.java
@@ -17,7 +17,64 @@ import org.mockito.Mockito;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(VertxExtension.class)package com.example;
+
+        import io.vertx.core.Future;
+        import io.vertx.core.json.JsonArray;
+        import io.vertx.ext.sql.ResultSet;
+        import io.vertx.ext.unit.junit.VertxExtension;
+        import io.vertx.junit5.VertxTestContext;
+        import io.vertx.junit5.VertxExtension;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.extension.ExtendWith;
+        import org.mockito.Mockito;
+        import static org.mockito.Mockito.*;
+
 @ExtendWith(VertxExtension.class)
+public class DBClientTest {
+
+    private DBClient dbClient;
+    private JDBCClient jdbcClient;
+
+    @BeforeEach
+    void setUp() {
+        jdbcClient = Mockito.mock(JDBCClient.class);
+        dbClient = new DBClient(jdbcClient);
+    }
+
+    @Test
+    void testCall(VertxTestContext testContext) {
+        // Mock connection and result set
+        SQLConnection sqlConnection = Mockito.mock(SQLConnection.class);
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+
+        // Mock a successful connection
+        when(jdbcClient.getConnection(any())).thenAnswer(invocation -> {
+            Handler<AsyncResult<SQLConnection>> handler = invocation.getArgument(0);
+            handler.handle(Future.succeededFuture(sqlConnection));
+            return null;
+        });
+
+        // Mock a successful query
+        when(sqlConnection.callWithParams(any(), any(), any(), any())).thenAnswer(invocation -> {
+            Handler<AsyncResult<ResultSet>> handler = invocation.getArgument(3);
+            handler.handle(Future.succeededFuture(resultSet));
+            return null;
+        });
+
+        // Params for the call function
+        JsonArray params = new JsonArray().add("param1").add("param2");
+        JsonArray outputs = new JsonArray().add("output1").add("output2");
+
+        dbClient.call("SELECT * FROM test WHERE id = ?", params, outputs)
+                .onComplete(testContext.succeeding(result -> testContext.verify(() -> {
+                    assertSame(resultSet, result);
+                    testContext.completeNow();
+                })));
+    }
+}
+
 public class DBClientTest {
 
     private DBClient dbClient;
